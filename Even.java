99123529128aadc81b2e50fd3868605a38a44b31
Nabil Niaz <nabilaniaz@gmail.com>
2015-03-30 23:24:15 -0400

    First

---
+++ Even.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+	int n=2;
+	while (n<=100){
+	System.out.println(n);
+	n=n+2;
+
+	}
+	
+
+}
+}
---
+++ Odd.java
@@ -0,0 +1,13 @@
+public class OddNumbers {
+
+	public static void main(String[] args) {
+	int n=1; int i=0;
+	while (n<=100){
+		System.out.println(n);
+		n=n+2;
+		
+	}
+
+	}
+
+}
